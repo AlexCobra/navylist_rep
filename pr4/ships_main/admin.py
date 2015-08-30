db59ceb90ed364873f460c1f34e3d66ed94ffb3f
alexcobra <AlexCobraMk3@mail.ru>
2015-08-30 22:59:14 +0600

    002

--- pr4/ships_main/admin.py
+++ pr4/ships_main/admin.py
@@ -1,16 +1,33 @@
 from django.contrib import admin
-from ships_main.models import ship_main, Builder, Power, Propulsion, Endurance, Commanders, ArmamentQuantity, Armaments, Armour, Devices, Pictures
+from ships_main.models import ship_main, Builder, Power, Propulsion, Endurance, Commanders, Armaments, ArmDevices, Armour, Devices, Pictures, Class, Type
 
+##class ArmamentQuantityInLine(admin.TabularInline):
+##    model = ArmamentQuantity
+##    extra = 1
+##
+##class ShipMainAdmin(admin.ModelAdmin):
+##    list_diplay = ('name', 'unique_name', 'builder', 'ordered', 'laid_down', 'launched',
+##                   'fate', 'ship_class', 'ship_type', 'displacement_norm', 'displacement_deep',
+##                   'length', 'beam', 'draught', 'power', 'propulsion', 'speed_surf', 'speed_sub',
+##                   'endurance', 'crew_peace', 'crew_war', 'commanders', 'armament', 'armour',
+##                   'devices', 'story', 'pictures')
+##    search_fields = ('name',)
+##    inlines = [ArmamentQuantityInLine,]
+##
+##class ArmamentsAdmin(admin.ModelAdmin):
+##    inline = (ArmamentQuantityInLine,)
+    
 admin.site.register(ship_main)
 admin.site.register(Builder)
 admin.site.register(Power)
 admin.site.register(Propulsion)
 admin.site.register(Endurance)
 admin.site.register(Commanders)
-admin.site.register(ArmamentQuantity)
 admin.site.register(Armaments)
+admin.site.register(ArmDevices)
 admin.site.register(Armour)
 admin.site.register(Devices)
 admin.site.register(Pictures)
+admin.site.register(Class)
+admin.site.register(Type)
 
-# Register your models here.
--- pr4/ships_main/models.py
+++ pr4/ships_main/models.py
@@ -1,119 +1,128 @@
 from django.db import models
 
 class ship_main(models.Model):
-        name = models.CharField(max_length=128, default=''),
-        unique_name = models.CharField(max_length=128, unique=True),
-        builder = models.ForeignKey('Builder'),
-        ordered = models.DateField(),
-        laid_down = models.DateField(),
-        launched = models.DateField(),
-        fate = models.DateField(),
-        ship_class = models.CharField(max_length=128),
-        displacement_norm = models.IntegerField(),
-        displacement_deep = models.IntegerField(),
-        length = models.IntegerField(),
-        beam = models.IntegerField(),
-        draught = models.IntegerField(),
-        power = models.ForeignKey('Power'),
-        propulsion = models.ForeignKey('Propulsion'),
-        speed_surf = models.IntegerField(),
-        speed_sub = models.IntegerField(),
-        endurance = models.ForeignKey('Endurance'),
-        crew_peace = models.IntegerField(),
-        crew_war = models.IntegerField(),
-        commanders = models.ForeignKey('Commanders'),
-        armament = models.ManyToManyField('Armament', through='ArmamentQuantity'),
-        armour = models.ForeignKey('Armour'),
-        devices = models.ForeignKey('Devices'),
-        story = models.TextField(),
-        pictures = models.ForeignKey('Pictures'),
+        name = models.CharField(max_length=128, default='')
+        unique_name = models.CharField(max_length=128, unique=True)
+        builder = models.ForeignKey('Builder', blank=True)
+        ordered = models.DateField(blank=True, default='1000-01-01')
+        laid_down = models.DateField(blank=True, default='1000-01-01')
+        launched = models.DateField(blank=True, default='1000-01-01')
+        fate = models.DateField(blank=True, default='1000-01-01')
+        ship_class = models.ForeignKey('Class')
+        ship_type = models.ForeignKey('Type', default='')
+        displacement_norm = models.IntegerField(blank=True)
+        displacement_deep = models.IntegerField(blank=True)
+        length = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+        beam = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+        draught = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+        power = models.ForeignKey('Power', blank=True, default='')
+        propulsion = models.ForeignKey('Propulsion', blank=True, default='')
+        speed_surf = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+        speed_sub = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+        endurance = models.ForeignKey('Endurance', blank=True)
+        crew_peace = models.IntegerField(blank=True)
+        crew_war = models.IntegerField(blank=True)
+        commanders = models.ForeignKey('Commanders', blank=True, null=True, default='')
+        armament = models.ForeignKey('Armaments', blank=True, null=True, default='')
+        armour = models.ForeignKey('Armour', blank=True, null=True, default='')
+        devices = models.ForeignKey('Devices', blank=True, null=True, default='')
+        story = models.TextField(blank=True, null=True, default='')
+        pictures = models.ForeignKey('Pictures', blank=True, null=True)
     
         def __str__(self):              # __unicode__ on Python 2
-                return (self.name, self.unique_name, self.ship_class)
+            return (self.unique_name)
 
 
 class Builder(models.Model):
-    name = models.CharField(max_length=128),
+    name = models.CharField(max_length=128)
+    founded = models.DateField(blank=True, default='1000-01-01')
+    defunct = models.DateField(blank=True, default='1000-01-01')
+    location = models.CharField(max_length=128, blank=True)
+    
 
     def __str__(self):              # __unicode__ on Python 2
-        return self.name
+        return (self.name)
 
 class Power(models.Model):
-    power = models.IntegerField(),
-    boilers = models.IntegerField(),
-    boilers_type = models.CharField(max_length=128),
-
-    def __str__(self):              # __unicode__ on Python 2
-        return self.boilers_type
+    power_description = models.TextField(blank=True)
 
 class Propulsion(models.Model):
-    shafts = models.IntegerField(),
-    steam_turbines = models.IntegerField(),
+    shafts = models.IntegerField(blank=True, null=True)
+    steam_turbines = models.IntegerField(blank=True, null=True)
 
 class Endurance(models.Model):
-    range1 = models.IntegerField(),
-    speed1 = models.IntegerField(),
-    range2 = models.IntegerField(),
-    speed2 = models.IntegerField(),
-    range3 = models.IntegerField(),
-    speed3 = models.IntegerField(),
+    range1 = models.IntegerField(blank=True, null=True)
+    speed1 = models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=2)
+    range2 = models.IntegerField(blank=True, null=True)
+    speed2 = models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=2)
+    range3 = models.IntegerField(blank=True, null=True)
+    speed3 = models.DecimalField(blank=True, null=True, max_digits=5, decimal_places=2)
 
 class Commanders(models.Model):
-    first_name = models.CharField(max_length=128),
-    second_name = models.CharField(max_length=128),
-    birthdate = models.DateField(),
-    income_date = models.DateField(),
-    withdraw_date = models.DateField,
-
-def __str__(self):              # __unicode__ on Python 2
-        return (self.first_name, self.second_name)
-
-class ArmamentQuantity(models.Model):
-    quantity = models.IntegerField(),
-    armament_type = models.ForeignKey('Armaments'),
-    ship_main = models.ForeignKey('ship_main'),
-
-class Armaments(models.Model):
-    device_type = models.CharField(max_length=128),
-    service_start_date = models.DateField(),
-    service_end_date = models.DateField(),
-    manufacturer = models.ForeignKey('Manufacturer'),
-    number_built = models.IntegerField(),
-    weight = models.IntegerField(),
-    length = models.IntegerField(),
-    calibre = models.IntegerField(),
-    recoil = models.IntegerField(),
-    rate_of_fire = models.IntegerField(),
-    velocity = models.IntegerField(),
-    max_firing_range = models.CharField(max_length=256),
-    story = models.TextField(),
+    first_name = models.CharField(max_length=128, blank=True)
+    second_name = models.CharField(max_length=128)
+    birthdate = models.DateField(blank=True, default='1000-01-01')
+    income_date = models.DateField(blank=True, default='1000-01-01')
+    withdraw_date = models.DateField(blank=True, default='1000-01-01')
 
     def __str__(self):              # __unicode__ on Python 2
-        return (self.device_type, self.max_firing_range, self.story)
+        return (self.second_name)
+
+class ArmDevices(models.Model):
+    device_type = models.CharField(max_length=128)
+    service_start_date = models.DateField(blank=True, default='1000-01-01')
+    service_end_date = models.DateField(blank=True, default='1000-01-01')
+    manufacturer = models.ForeignKey('Builder', blank=True, null=True)
+    number_built = models.IntegerField(blank=True, null=True)
+    weight = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+    length = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+    calibre = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+    recoil = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+    rate_of_fire = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+    velocity = models.DecimalField(blank=True, max_digits=5, decimal_places=2, null=True)
+    max_firing_range = models.CharField(max_length=256, blank=True)
+    story = models.TextField(blank=True)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return (self.device_type)
 
 class Armour(models.Model):
-    belt = models.IntegerField(),
-    deck = models.IntegerField(),
-    barbettes = models.IntegerField(),
-    gun_turrets = models.IntegerField(),
-    conning_tower = models.IntegerField(),
-    bulkheads = models.IntegerField(),
-    magazines = models.IntegerField(),
+    belt = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+    deck = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+    barbettes = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+    gun_turrets = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+    conning_tower = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+    bulkheads = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
+    magazines = models.DecimalField(blank=True, max_digits=5, decimal_places=2)
 
 class Devices(models.Model):
-    device_type = models.CharField(max_length=128),
-    description = models.TextField(),
+    device_type = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
 
     def __str__(self):              # __unicode__ on Python 2
-        return (self.device_type, self.description)
+        return self.device_type
 
 class Pictures(models.Model):
-    picture_name = models.CharField(max_length=128),
-    picture = models.ImageField(upload_to='/ships_main', max_length=200),
-    picture_source = models.URLField(),
+    picture_name = models.CharField(max_length=128, blank=True)
+    picture = models.ImageField(upload_to='/ships_main', max_length=200)
+    picture_source = models.URLField()
 
     def __str__(self):              # __unicode__ on Python 2
         return self.picture_name
 
+class Armaments(models.Model):
+    ship_id = models.OneToOneField(ship_main)
+    quantity = models.IntegerField()
+    armament_type = models.ForeignKey('ArmDevices', blank=False)
 
+class Class(models.Model):
+        class_data = models.CharField(max_length=128)
+        
+        def __str__(self):              # __unicode__ on Python 2
+            return self.class_data
 
+class Type(models.Model):
+        class_type = models.CharField(max_length=128)
+
+        def __str__(self):              # __unicode__ on Python 2
+            return self.class_type
